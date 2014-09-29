67960e6e463495d3df7f2d4bb0c733c27583cc16
Robert Brewer <fumanchu@aminus.org>
2014-09-29 11:47:12 -0700

    Adding the crunch:error Element subclass.

--- pycrunch/__init__.py
+++ pycrunch/__init__.py
@@ -11,7 +11,12 @@ __all__ = [
     'elements',
     'shoji',
     'importing',
-    'ClientError', 'ServerError',
+    'ClientError', 'ServerError', 'CrunchError'
     'Session',
     'urljoin'
 ]
+
+
+class CrunchError(elements.Element):
+
+    element = "crunch:error"
