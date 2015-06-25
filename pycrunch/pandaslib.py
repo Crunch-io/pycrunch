c519df6d3bb50a676e61f3cd1950be103b092eb1
percious <chris@percious.com>
2015-06-25 14:43:25 -0700

    [#97832884]: individual columns, datetime support

--- pycrunch/pandaslib.py
+++ pycrunch/pandaslib.py
@@ -1,38 +1,51 @@
-from pandas import DataFrame, Categorical
+from pandas import DataFrame, Categorical, Series, to_datetime
 
 
-def clean_item(item):
-    try:
-        item['?']
-        return None
-    except (TypeError, KeyError):
-        return item
+def series_from_variable(value, metadata):
+    value = [None if (isinstance(item, dict) and item.keys() == ['?']) else item for item in value]
 
-def clean_value(k, v, metadata=None):
-    return [clean_item(item) for item in v]
+    type_ = metadata.type
+    if type == 'categorical':
+        cats = {cat['id']:cat['name'] for cat in metadata['categories']}
+        value = [None if val is None else cats[val] for val in value]
+        return Categorical(value,
+                           categories=[cat['name'] for cat in metadata['categories'] if not cat['missing']],
+                           ordered=True)
 
-def dataframe_from_dataset(site, dataset_name_or_id):
+    if type_ == 'datetime':
+        return to_datetime(Series(value))
+
+    return Series(value)
+
+
+def dataframe_from_dataset(site, dataset_name_or_id, variables=None):
 
     try:
         dataset = site.datasets.by('name')[dataset_name_or_id]
     except KeyError:
         dataset = site.datasets.by('id')[dataset_name_or_id]
 
-    t = dataset.entity.table
     data = {}
 
-    for k, v in t.data.iteritems():
-        metadata = t.metadata[k]
-        value = clean_value(k, v, metadata)
-        print metadata.name, metadata.type
-        if metadata.type == 'categorical':
-            cats = {cat['id']:cat['name'] for cat in metadata['categories']}
-            value = [None if val is None else cats[val] for val in value]
-            data[metadata.name] = Categorical(value,
-                                              categories=[cat['name'] for cat in metadata['categories'] if not  cat['missing']],
-                                              ordered=True)
-            continue
+    if variables is None:
 
-        data[metadata.name] = value
+        t = dataset.entity.table
+
+        for name, value in t.data.iteritems():
+            metadata = t.metadata[name]
+            data[metadata.name] = series_from_variable(value, metadata)
+
+    else:
+        if not isinstance(variables, list):
+            variables = list(variables)
+
+        for variable in variables:
+            try:
+                t = dataset.entity.variables.by('name')[variable].entity
+            except KeyError:
+                raise KeyError('No variable with name: %s' % variable)
+            metadata = t.body
+            value = t.values_url.value
+            data[metadata.name] = series_from_variable(value, metadata)
 
     return DataFrame(data)
