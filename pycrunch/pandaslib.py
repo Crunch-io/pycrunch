5d4c621b12d8b28ce93038a84f22e66cc23a10dd
percious <chris@percious.com>
2015-06-25 12:23:19 -0700

    [#97832884]: categoricals work.

--- pycrunch/pandaslib.py
+++ pycrunch/pandaslib.py
@@ -1,5 +1,15 @@
-from pandas import DataFrame
+from pandas import DataFrame, Categorical
 
+
+def clean_item(item):
+    try:
+        item['?']
+        return None
+    except (TypeError, KeyError):
+        return item
+
+def clean_value(k, v, metadata=None):
+    return [clean_item(item) for item in v]
 
 def dataframe_from_dataset(site, dataset_name_or_id):
 
@@ -8,6 +18,21 @@ def dataframe_from_dataset(site, dataset_name_or_id):
     except KeyError:
         dataset = site.datasets.by('id')[dataset_name_or_id]
 
-    df = DataFrame(dataset.entity.table)
+    t = dataset.entity.table
+    data = {}
 
-    return df
+    for k, v in t.data.iteritems():
+        metadata = t.metadata[k]
+        value = clean_value(k, v, metadata)
+        print metadata.name, metadata.type
+        if metadata.type == 'categorical':
+            cats = {cat['id']:cat['name'] for cat in metadata['categories']}
+            value = [None if val is None else cats[val] for val in value]
+            data[metadata.name] = Categorical(value,
+                                              categories=[cat['name'] for cat in metadata['categories'] if not  cat['missing']],
+                                              ordered=True)
+            continue
+
+        data[metadata.name] = value
+
+    return DataFrame(data)
