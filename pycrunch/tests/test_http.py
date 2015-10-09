8f6c721ad8855ba5ea4379ecfe47d0e616f07f8c
Neal Richardson <neal@crunch.io>
2015-10-09 11:11:26 -0700

    Remove print

--- pycrunch/tests/test_http.py
+++ pycrunch/tests/test_http.py
@@ -9,7 +9,6 @@ class PycrunchTestCase(TestCase):
         r = s.get("http://httpbin.org/headers")
         req_headers_sent = r.request.headers
         req_headers_received = r.json()['headers']
-        print req_headers_sent, req_headers_received
         self.assertTrue('user-agent' in req_headers_sent)
         self.assertTrue('User-Agent' in req_headers_received)
         self.assertTrue('pycrunch' in req_headers_sent.get('user-agent', ''))
