03dda729e0da1f34fcdb4ef110c3b816d560963c
Robert Brewer <fumanchu@aminus.org>
2016-06-03 14:44:11 -0700

    New test for gzip request header, plus a version bump.

--- pycrunch/tests/test_http.py
+++ pycrunch/tests/test_http.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 
-import pytest
 import requests
 from pycrunch import Session, __version__
 from pycrunch.lemonpy import ServerError
@@ -15,26 +14,36 @@ except ImportError:
     import mock
 
 
-class PycrunchTestCase(TestCase):
-    
-    def test_user_agent(self):
+class TestHTTPRequests(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.s = Session("not an email", "not a password")
+        cls.r = cls.s.get("http://httpbin.org/headers")
+
+    def test_request_sends_user_agent(self):
         pycrunch_ua = 'pycrunch/%s' % __version__
-        s = Session("not an email", "not a password")
-        r = s.get("http://httpbin.org/headers")
-        req_headers_sent = r.request.headers
-        req_headers_received = r.json()['headers']
+        req_headers_sent = self.r.request.headers
+        req_headers_received = self.r.json()['headers']
         self.assertTrue('user-agent' in req_headers_sent)
         self.assertTrue('User-Agent' in req_headers_received)
         self.assertTrue(pycrunch_ua in req_headers_sent.get('user-agent', ''))
         self.assertTrue(pycrunch_ua in req_headers_received.get('User-Agent', ''))
 
-    def test_response_with_no_content_type_header(self):
-        """
-        Simulate a 504 response with no Content-Type header and empty body.
+    def test_request_sends_gzip(self):
+        req_headers_sent = self.r.request.headers
+        req_headers_received = self.r.json()['headers']
+        self.assertIn("gzip", req_headers_sent['Accept-Encoding'])
+        self.assertIn("gzip", req_headers_received['Accept-Encoding'])
 
-        pycrunch.lemonpy.ResponseHandler should be able to cope with this
-        peculiar situation.
-        """
+
+class TestHTTPResponses(TestCase):
+
+    def test_response_with_no_content_type_header(self):
+        # Simulate a 504 response with no Content-Type header and empty body.
+
+        # pycrunch.lemonpy.ResponseHandler should be able to cope with this
+        # peculiar situation.
 
         def _resp(adapter, request, *args, **kwargs):
             headers = {'Content-Length': '0'}
@@ -45,10 +54,10 @@ class PycrunchTestCase(TestCase):
 
         patch.start()
         s = Session("not an email", "not a password")
-        with pytest.raises(ServerError) as exc_info:
+        with self.assertRaises(ServerError) as exc_info:
             s.get("http://httpbin.org/status/504")
         patch.stop()
 
-        response = exc_info.value.args[0]
+        response = exc_info.exception.args[0]
         assert isinstance(response, requests.models.Response)
         self.assertEqual(response.status_code, 504)
