1d96127f7468a68386b33f014302469139fd1985
Robert Brewer <fumanchu@aminus.org>
2020-12-14 13:58:43 -0800

    Oops

--- src/pycrunch/shoji.py
+++ src/pycrunch/shoji.py
@@ -12,7 +12,7 @@ from six.moves import urllib
 import six
 
 from pycrunch import elements
-from berrypy import URL, ClientError, ServerError
+from pycrunch.lemonpy import URL, ClientError, ServerError
 
 
 class Tuple(elements.JSONObject):
